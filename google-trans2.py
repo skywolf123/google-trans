30b3e16e048ac03eb4e2a171da09d4790ff61e0b
skywolf123 <z4458622z>
2019-03-06 10:55:16 +0800

    first commit

--- google-trans2.py
+++ google-trans2.py
@@ -23,56 +23,53 @@ def work(urls):
 
 
 def total_translate():
-    file2 = open('de2en_en.txt', mode='a', encoding='utf-8')
+    file2 = open('en.txt', mode='w', encoding='utf-8')
 
-    with open('de.txt', mode='r', encoding='utf-8') as f:
+    with open('cn.txt', mode='r', encoding='utf-8') as f:
         urls = []
         num = 0
         for line in f:
-            num += 1
-
             line = line.strip()
             token = translator.token_acquirer.do(line)
-            url = "https://translate.google.cn/translate_a/single?client=t&sl=de&tl=en&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=3&tsel=0&kc=1&tk={0}&q={1}".format(
+            url = "https://translate.google.cn/translate_a/single?client=t&sl=zh-cn&tl=en&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=3&tsel=0&kc=1&tk={0}&q={1}".format(
                 token, line)
             urls.append(url)
 
-            if len(urls) >= 50:
-                res = work(urls)
-                for r in res:
-                    if hasattr(r, 'status_code'):
-                        if r.status_code == 200:
-                            try:
-                                a = format_json(r.text)
-                                target = ''.join([d[0] if d[0] else '' for d in a[0]])
-                                source = ''.join([d[1] if d[1] else '' for d in a[0]])
-                            except Exception as e:
-                                logger.error('when format:%s', e)
-                                logger.error('%s\n%s', r.text)
-                                source = ''
-                                target = ''
-                            if len(source) != 0 and len(target) != 0:
-                                file2.write(target + '\n')
-                            else:
-                                file2.write('\n')
-                        else:
-                            file2.write('\n')
-                urls = []
-                logger.info('finish 50 sentence, now at %s', num)
+        res = work(urls)
+        for r in res:
+            num += 1
+            if hasattr(r, 'status_code'):
+                if r.status_code == 200:
+                    try:
+                        a = format_json(r.text)
+                        target = ''.join([d[0] if d[0] else '' for d in a[0]])
+                        source = ''.join([d[1] if d[1] else '' for d in a[0]])
+                    except Exception as e:
+                        logger.error('when format:%s', e)
+                        logger.error('%s\n%s', r.text)
+                        source = ''
+                        target = ''
+                    if len(source) != 0 and len(target) != 0:
+                        file2.write(target + '\n')
+                    else:
+                        file2.write('\n')
+                else:
+                    file2.write('\n')
+            logger.info('finish %s sentence, now at %s', len(res), num)
     file2.close()
 
 
 def sentence_translate(line):
     line = line.strip()
-    text = translator.translate(line, src='de', dest='en').text
+    text = translator.translate(line, src='zh-cn', dest='en').text
     return text
 
 
 def complete_translate():
-    file1 = open('de2en_en.txt', mode='r', encoding='utf-8')
-    file2 = open('new_de2en_en.txt', mode='a', encoding='utf-8')
+    file1 = open('en.txt', mode='r', encoding='utf-8')
+    file2 = open('new_en.txt', mode='w', encoding='utf-8')
     i = 1
-    with open('de.txt', mode='r', encoding='utf-8') as f:
+    with open('cn.txt', mode='r', encoding='utf-8') as f:
         for line in f:
             t = file1.readline()
             if len(t) == 1:  # 'only \n'
